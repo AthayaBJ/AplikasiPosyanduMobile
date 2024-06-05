a3c442295fea435c2422c88ac9960aaab2e63ef0
AthayaBJ <AthayaBJ@gmail.com>
2024-06-05 20:45:12 +0700

    Profile Fragment Container

--- app/src/main/java/com/example/aplikasiposyandu_mobile/ProfileFragment.java
+++ app/src/main/java/com/example/aplikasiposyandu_mobile/ProfileFragment.java
@@ -1,64 +1,92 @@
 package com.example.aplikasiposyandu_mobile;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private DatabaseReference mDb = FirebaseDatabase.getInstance().getReference();
+    SharedPreferences sharedPreferences;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        TextView nama_akun = view.findViewById(R.id.nama_akun);
+        TextView email_akun = view.findViewById(R.id.email_akun);
+        Button btn_logout = view.findViewById(R.id.logout);
+        Button dashboard = view.findViewById(R.id.dashboard);
+        Button ganti_pass = view.findViewById(R.id.ganti_pass);
+
+        sharedPreferences = getActivity().getSharedPreferences("myPref", Context.MODE_PRIVATE);
+        String role = sharedPreferences.getString("role", "");
+        btn_logout.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.clear();
+            editor.apply();
+
+            startActivity(intent);
+            getActivity().finish();
+
+        });
+
+        ganti_pass.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), ChangePasswordActivity.class);
+            startActivity(intent);
+        });
+
+        dashboard.setOnClickListener(v -> {
+            if (role.equals("Agency")) {
+                Log.d("Role", "sharedpreferences: " + role);
+                Intent intent = new Intent(getActivity(), DashboardActivity.class);
+                startActivity(intent);
+            }else {
+                Log.d("Role", "sharedpreferences: " + role);
+                Intent intent = new Intent(getActivity(), DashboardRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        try{
+            String nama = sharedPreferences.getString("nama", "");
+            String email = sharedPreferences.getString("email", "");
+            nama_akun.setText(nama);
+            email_akun.setText(email);
+//            if (role != "agency"){
+//                dashboard.setText("Daftar bimbel");
+//                Log.d("role", "onCreateView: "+role);
+//            }
+
+        }catch(Exception e){
+            Log.e("Data error", String.valueOf(e));
+        }
+
+        return view;
     }
+
 }
