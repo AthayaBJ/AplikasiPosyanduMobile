c5f40c8017ca78836a5e4b8ba902492f4dc19bb6
Jagasaki69 <fireanndice384@gmail.com>
2024-06-10 09:51:00 +0700

    tambah lokasi ng .xml imunisasi

--- app/src/main/java/com/example/aplikasiposyandu_mobile/Imunisasi.java
+++ app/src/main/java/com/example/aplikasiposyandu_mobile/Imunisasi.java
@@ -1,14 +1,44 @@
 package com.example.aplikasiposyandu_mobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 public class Imunisasi extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imunisasi);
+
+        EditText namaAnak = findViewById(R.id.namaAnak);
+        EditText umur = findViewById(R.id.umur);
+        EditText nik = findViewById(R.id.nik);
+        EditText tanggalImunisasi = findViewById(R.id.tanggalImunisasi);
+        Spinner lokasiSpinner = findViewById(R.id.lokasiSpinner);
+        Button submitButton = findViewById(R.id.submitButton);
+
+        // setting spiner e ben iso luru lokasine
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.lokasi_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lokasiSpinner.setAdapter(adapter);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama = namaAnak.getText().toString();
+                String umurAnak = umur.getText().toString();
+                String nikAnak = nik.getText().toString();
+                String tanggal = tanggalImunisasi.getText().toString();
+                String lokasi = lokasiSpinner.getSelectedItem().toString();
+
+                // Aksi ketika tombol Submit ditekan
+                Toast.makeText(Imunisasi.this, "Data: " + nama + ", " + umurAnak + ", " + nikAnak + ", " + tanggal + ", " + lokasi, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
